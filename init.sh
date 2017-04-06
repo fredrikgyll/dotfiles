704ff4f0f5dc32f4bbd0bdf21f4a778e4add7dd3
fredikgyll <fredrik.gyllenhammar@ntnu.no>
2017-04-06 18:31:18 +0200

    fix everything

--- init.sh
+++ init.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
+cd $HOME/dotfiles/
 #Install oh-my-zsh if it isnt already
 if [ ! -d "$HOME/.oh-my-zsh" ]
 then
-          # Control will enter here if $DIRECTORY doesn't exist.
             sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
     fi
 #create symlinks
-ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
-ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
-ln -s $HOME/dotfiles/zshcustom/aliases.zsh $ZSH_CUSTOM/aliases.zsh
+for file in `ls -A HOME/`
+do
+        ln -fs $HOME/dotfiles/HOME/$file $HOME/$file
+done
+for file in `ls -A ZSH_CUSTOM`
+do
+	ln -fs $HOME/dotfiles/ZSH_CUSTOM/$file $ZSH_CUSTOM/$file
+done
 mkdir -p $HOME/.vim/colors
-ln -s $HOME/dotfiles/.vim/colors/darcula.vim $HOME/.vim/colors/darcula.vim
+ln -fs $HOME/dotfiles/.vim/colors/darcula.vim $HOME/.vim/colors/darcula.vim
 source $HOME/.zshrc
 echo "init complete"
 
