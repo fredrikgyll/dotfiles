bf645ebff27ac6b8edae95789f21323994d2dc6e
Fredrik Gyllenhammar <fredrik.gyllenhammar@ntnu.no>
2018-12-19 13:31:54 +0100

    Add mac-only block to init

--- init.sh
+++ init.sh
@@ -1,25 +1,37 @@
 #!/bin/zsh
-cd $HOME/dotfiles/
-
-#Install oh-my-zsh if it isnt already
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/loket/oh-my-zsh/feature/batch-mode/tools/install.sh)" -s --batch || {
-  echo "Could not install Oh My Zsh" >/dev/stderr
-    exit 1
-    }
+cd "${0%/*}"
 
 #make hidden files globbable
 setopt glob_dots
 
 #create symlinks for $HOME
 ln -fs $HOME/dotfiles/home/* $HOME/
-#create symlinks for $ZSH_CUSTOM
-ln -fs $HOME/dotfiles/zsh_custom/* $ZSH_CUSTOM/
 
-if [ ! -d /home/.vim/bundle/Vundle.vim/.git ]
-then
-        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [[ ! -d /home/.vim/bundle/Vundle.vim/.git ]]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+echo "Installing all vim plugins."
+vim +PluginInstall +qall
+
+if [ "$(uname)" == "Darwin" ]; then
+  echo "MacOS detected."
+  which -s brew
+  if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    echo "Homebrew is not installed, installing now."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+
+  if type xcode-select >&- && xpath=$( xcode-select --print-path ) &&
+    test -d "${xpath}" && test -x "${xpath}" ; then
+    echo "XCode CLI tools are installed and configured correctly."
+  else
+    echo "XCode CLI tools are not installed, starting installer now."
+    xcode-select --install
+  fi
 fi
 
 source $HOME/.zshrc
-echo "init complete"
+echo "init complete!"
 
